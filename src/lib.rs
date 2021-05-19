9091284ffa8728c6cf07d6574c370bedfa033348
kachark <kkachar@hotmail.com>
2021-05-19 10:06:20 -0400

    updated test_dynamics test to simulate dynamics evolution over time

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@ mod tests {
         // TODO: this is basically a System in the ECS sense (solving the dynamics and controller)
         // combined with a 10 tick game loop
 
+        use super::math::euler::MidPointEuler;
         use super::dynamics::controls::lqr::LinearQuadraticRegulator as LQR;
         use super::dynamics::linear_system::linear_dynamics::*;
         use super::na::{DMatrix, DVector};
@@ -36,7 +37,7 @@ mod tests {
         let C = A.clone();
         let D = B.clone();
 
-        let Q = DMatrix::<f32>::identity(2, 2) * 1E-1;
+        let Q = DMatrix::<f32>::identity(2, 2);
         let R = DMatrix::from_vec(1, 1, vec![1.]);
 
         let double_integrator = LinearSystem::new(A, B, C, D);
@@ -47,17 +48,43 @@ mod tests {
             R,
         );
 
-        let mut x = DVector::from_vec(vec![10., 10.]);
+        let x0 = DVector::from_vec(vec![10., 10.]);
         let (K, _P) = match lqr.solve() {
             Ok((value1, value2)) => (value1, value2),
             Err(_) => (DMatrix::<f32>::zeros(1, 1), DMatrix::<f32>::zeros(1, 1)),
         };
 
-        for _ in 0..10 {
-            let u = -&K * &x;
-            x = double_integrator.f(0f32, &x, Some(&u));
+        let mut trajectory: Vec<DVector<f32>> = vec![x0];
+        let step = 0.1;
+        let t0 = 0;
+        let tf = 10;
+        for t in t0..tf {
 
-            println!("{:?}", &x);
+            let x_prev = trajectory[trajectory.len()-1].clone();
+
+            // Wrap dynamics/controls in appropriately defined closure - f(t, x)
+            let f = |t: f32, x: &DVector<f32>| {
+                let u = -&K * x;
+                double_integrator.f(t, x, Some(&u))
+            };
+
+            // Integrate dynamics
+            let (_t_history, traj) = MidPointEuler(t as f32, x_prev, step, (t as f32)+1f32, f);
+
+            // Store result
+            for state in traj {
+
+                trajectory.push(state);
+
+            }
+
         }
+
+        for x in trajectory {
+
+            println!("{:?}", &x.data);
+
+        }
+
     }
 }
